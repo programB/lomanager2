53fd4e877abc2c35edfe52664e0bd5d9a98f754f
programB <35240807+programB@users.noreply.github.com>
2023-05-27 23:17:09 +0200

    adds missing stub functions to the skeleton of the program

--- lomanager2.py
+++ lomanager2.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import pathlib
 
 
 def get_current_locale() -> str:
@@ -41,6 +42,67 @@ def get_system_information() -> dict:
     return system_information
 
 
+def install_LibreOffice(dir_path: pathlib.Path, install_type: str) -> int:
+    """Runs normal installation procedure of LibreOffice.
+
+    The procedure checks the system for requirements
+    that must be met for LibreOffice installation.
+    It then either downloads required packages from
+    the internet and puts them to the dir_path,
+    verifies and installs them
+    or installs from locally stored packages.
+    For "normal" mode the OS must be fully updated for the
+    procedure to succeed,
+    for "local-copy" install this check is skipped.
+
+    Parameters
+    ----------
+    dir_path : pathlib.Path or str
+        A path to an existing directory to where packages will be downloaded
+        and installed from or where they are already present.
+
+    install_type : {"normal", "local-copy"}
+        Determines type of install. "normal" means LibreOffice
+        packages will be downloaded from the internet
+        if set to "local-copy" locally stored packages will be used.
+
+    Returns
+    -------
+    install_status : int
+       status code
+    """
+
+    install_status = -1
+    logging.debug(
+        f"Not implemented.\n"
+        f'Value passed: "{dir_path}" '
+        f'type: "{type(dir_path)}"\n'
+        f'Value passed: "{install_type}" '
+        f'type: "{type(install_type)}"\n'
+        f'Value returned: "{install_status}" '
+        f'type: "{type(install_status)}"\n'
+    )
+    return install_status
+
+
+def uninstall_LibreOffice() -> int:
+    """Removes LibreOffice from the system.
+
+    Returns
+    -------
+    uninstall_status : int
+       status code
+    """
+
+    uninstall_status = -1
+    logging.debug(
+        f"Not implemented.\n"
+        f'Value returned: "{uninstall_status}" '
+        f'type: "{type(uninstall_status)}"\n'
+    )
+    return uninstall_status
+
+
 def main():
     # Some logging useful to debug this script -
     # - not related to lomanger2 flowchart
@@ -75,13 +137,20 @@ def main():
                 print("invalid option")
 
         if choice == 1:
-            normal_install = True
             print("Begin normal installation procedure... ")
+            install_dir = pathlib.Path("~/tmp/")
+            install_status = install_LibreOffice(
+                dir_path=install_dir, install_type="normal"
+            )
         elif choice == 2:
-            normal_install = False
             print("Begin installation from local copy... ")
+            install_dir = pathlib.Path("~/tmp/")
+            install_status = install_LibreOffice(
+                dir_path=install_dir, install_type="local-copy"
+            )
         elif choice == 3:
             print("Begin uninstall procedure... ")
+            uninstall_LibreOffice()
         else:
             time_to_quit = True
 
