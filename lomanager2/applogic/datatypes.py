5b2070aade1c2bbd328a4c416859d8b3682212d7
programB <35240807+programB@users.noreply.github.com>
2023-06-29 19:18:51 +0200

    Moves VirtualPacakge definition into separate file
    See #48

---
+++ lomanager2/applogic/datatypes.py
@@ -0,0 +1,90 @@
+class VirtualPackage(object):
+    """VirtualPackage represents a bundle of rpm packages operated as one
+
+    A bundle is one or more rpm packages that are or should be
+    installed/uninstalled together without side effects.
+    Such a VirtualPackage is a unit on package selection logic operates.
+
+    Each VirtualPackage objects holds the information of what real
+    rpm packages it represents and how much disk space each of them
+    occupies when installed.
+
+    There are 2 kinds of these virtual packages / bundles:
+        - "core-packages" is the package representing OpenOffice
+        or LibreOffice base package and base help package.
+        These are always installed/removed together and provide required
+        and sufficient functionality to use the Office suite.
+
+        This kind of package also represents a single rpm package with
+        Clipart library.
+
+        - "<language code>" this virtual package represents a bundle of
+        localization rpm packages for specific language that is
+        a language pack and a language help pack.
+
+    Each virtual package has a state, represented by a number of
+    flags/attributes. These describe whether:
+        - the package was marked for specific operation
+          (install/removal/upgrade)
+        - if it can be marked for such operation,
+        - should the operation be visible to the user and
+        - should it be in enabled state
+          (or disabled even if it is visible).
+
+    Attributes
+    ----------
+    kind : str
+    family :str
+    version : str
+    real_packages = list[dict[str, int]]
+    is_removable : bool
+    is_remove_opt_visible : bool
+    is_remove_opt_enabled : bool
+    is_marked_for_removal : bool
+    is_upgradable : bool
+    is_upgrade_opt_visible : bool
+    is_upgrade_opt_enabled : bool
+    is_marked_for_upgrade : bool
+    is_installable : bool
+    is_install_opt_visible : bool
+    is_install_opt_enabled : bool
+    is_marked_for_install : bool
+    """
+
+    def __init__(self, kind: str, family: str, version: str) -> None:
+        """Creates VirtualPackage object
+
+        Parameters
+        ----------
+        kind : str
+            The type of virtual package to create: "core-packages"
+            or a specific language code like. "jp" or "fr"
+
+        family : str
+            Software this virtual package represents: "OpenOffice",
+            "LibreOffice" or "Clipart"
+
+        version : str
+            Version of the package. Dot separated format eg. "2.4.1"
+        """
+
+        self.kind = kind
+        self.family = family
+        self.version = version
+        self.real_packages = [{"rpm name": "", "size": 0}]  # size in kilobytes
+        # Remove flags
+        self.is_removable = False
+        self.is_remove_opt_visible = False
+        self.is_remove_opt_enabled = False
+        self.is_marked_for_removal = False
+        # Upgrade flags
+        self.is_upgradable = False
+        self.is_upgrade_opt_visible = False
+        self.is_upgrade_opt_enabled = False
+        self.is_marked_for_upgrade = False
+        # Install flags
+        self.is_installable = False
+        self.is_install_opt_visible = False
+        self.is_install_opt_enabled = False
+        self.is_marked_for_install = False
+
