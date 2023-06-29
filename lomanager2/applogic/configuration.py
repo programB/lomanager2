da4342d25afd5df33b1f0fb4b96427e13d0e0f07
programB <35240807+programB@users.noreply.github.com>
2023-06-29 21:43:02 +0200

    Links GUI with adapter and logic. Working code.
    See #65

--- lomanager2/applogic/configuration.py
+++ lomanager2/applogic/configuration.py
@@ -1,5 +1,9 @@
 import logging
 
+lomanger2_version = "7.5"
+latest_available_LO_version = "7.5"
+latest_available_clipart_version = "5.8"
+
 logging.basicConfig(
     format="[%(levelname)s](%(asctime)s) (in %(funcName)s): %(message)s",
     level=logging.DEBUG,
