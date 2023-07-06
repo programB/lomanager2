61237a001e41a65e7d7bcdde6eb08c4672e7c094
programB <35240807+programB@users.noreply.github.com>
2023-07-06 20:18:53 +0200

    Fixes worker thread implementation...
    ...Now thread is created in one of Adapter's slot methods on demand (not in the __init__).
    This allows sending up to date parameters set by the user in the GUI.
    The thread worker now runs only a single method of the MainLogic to apply changes but passes on the
    installation mode as a variable (this depends on user selection in the GUI).
    Underlying MainLogic method is not yet modified to support this.

--- lomanager2/adapters/qt_adapter.py
+++ lomanager2/adapters/qt_adapter.py
@@ -10,7 +10,6 @@ from PySide6.QtCore import (
 )
 
 from applogic.packagelogic import MainLogic
-from applogic.subprocedures import install as install_subprocedure
 from gui import AppMainWindow, ProgressDialog
 from viewmodels import PackageMenuViewModel
 from threads import InstallProcedureWorker
@@ -20,7 +19,7 @@ class Adapter(QObject):
     # register custom "refresh" signal (no data passed with it)
     # This is a class attribute (defined before outside __init__)
     refresh = Signal()
-    start_apply = Signal()
+    run_install_in_mode = Signal(str)
 
     def __init__(self, app_main_model, app_main_view) -> None:
         super().__init__()  # this is important when registering custom events
@@ -37,7 +36,7 @@ class Adapter(QObject):
         # TODO: adding this extra window just for test
         #       it should be rather defined directly in main_window
         #       like the lang modal
-        self.progress_view = ProgressDialog(parent=self._main_view)
+        self._progress_view = ProgressDialog(parent=self._main_view)
 
         # The viewmodel (PackageMenuViewModel) for the object responsible
         # for dealing with packages selection _main_view
@@ -52,25 +51,8 @@ class Adapter(QObject):
         # TODO: Does not exist yet - Implement
         # extra_langs_menu_viewmodel = LangsMenuViewModel()
 
-        # Prepare separate threads that run: apply_changes and
-        # install_from_local_copy functions
-        # from the MainLogic object (don't start them yet though)
-
-        # TODO: passing _keep_packages like causes the Worker
-        #       to always have the value of _keep_packages = False,
-        #       any future updates of this variable are not
-        #       noticed. Worked MUST to be created when it's ready
-        #       to be started (or have methods of passing data to it
-        #       while it's already started which would
-        #       be unnecessarily complicated in this case.)
-        self._keep_packges = False
-        self.thread_apply_changes = InstallProcedureWorker(
-            self._main_model.apply_changes,
-            keep_packages=self._keep_packges,
-        )
-        self.thread_local_copy_install = InstallProcedureWorker(
-            self._main_model.install_from_local_copy
-        )
+        self._keep_packages = False
+        self._local_copy_folder = None
 
         self._bind_views_to_viewmodels()
         # For the extra buttons outside table views I don't know how to
@@ -89,13 +71,8 @@ class Adapter(QObject):
     def _connect_signals_and_slots(self):
         # TODO: Will clicking this button at wrong times result in unintended
         #       consequences? Should this be prevented by disabling this button?
-        self._main_view.button_apply_changes.clicked.connect(self._apply_changes)
-        self.thread_apply_changes.progress.connect(self._progress_was_made)
-        self.thread_apply_changes.finished.connect(self._thread_stopped_or_terminated)
-        # TODO: connecting to terminate just for tests
-        # self.progress_view.button_terminate.clicked.connect(self.thread_apply_changes.stop)
-        self.progress_view.button_terminate.clicked.connect(
-            self.thread_apply_changes.terminate
+        self._main_view.button_apply_changes.clicked.connect(
+            self._apply_changes_was_requested
         )
 
         # TODO: And there are buttons inside that modal window.
@@ -114,58 +91,75 @@ class Adapter(QObject):
 
         # TODO: test connect "refresh" (custom signal)
         self.refresh.connect(self._do_something_on_refresh)
-        # TODO: is the refresh button needed? Connecting it for now
-        #       to the same slot as the "refresh" signal which is intended
-        #       for automatic (not manual) emission.
+
         self._main_view.button_install_from_local_copy.clicked.connect(
-            self._install_from_local_copy
-        )
-        self.thread_local_copy_install.finished.connect(
-            self._thread_stopped_or_terminated
+            self._install_from_local_copy_was_requested
         )
 
         # Keep packages checkbox
-        self._main_view.confirm_apply_view.checkbox_keep_packages.stateChanged.connect(self._set_keep_packages_state) 
-        # self._main_view.checkbox_keep_packages.stateChanged.connect(
-        #     self._set_keep_packages_state
-        # )
-        self.start_apply.connect(self._start_apply_changes_subprocedure)
+        self._main_view.confirm_apply_view.checkbox_keep_packages.stateChanged.connect(
+            self._set_keep_packages_state
+        )
+        self.run_install_in_mode.connect(self._start_apply_changes_subprocedure)
 
     def _do_something_on_refresh(self):
         print("Refreshing!")
 
-    def _apply_changes(self):
+    def _apply_changes_was_requested(self):
         if self._main_view.confirm_apply_view.exec():
-            self.start_apply.emit()
+            print("Applying changes...")
+            self.run_install_in_mode.emit("network_install")
         else:
-            print("Aborted")
+            print("Cancel clicked: User decided not to apply changes.")
 
-    def _start_apply_changes_subprocedure(self):
-        print("Apply Accepted")
-        # self.progress_view.show()
-        # self.thread_apply_changes.start()  # starts the prepared thread
+    def _install_from_local_copy_was_requested(self):
+        # TODO: - Add local copy dialog to GUI to allow the user
+        #       to point to a folder with saved packages and
+        #       - Open this dialog here (in which the user
+        #         will set folder variable accoringly) (ADD IT to self.)
+        #       - emit signal to start self._start_apply_changes_subprocedure()
+        #          (and that method should use folder path stored in variable
+        #          to pass it to the worker thread).
+        # TODO: dummy call for now
+        print("Install from local copy")
+        self.run_install_in_mode.emit("local_copy_install")
 
-    def _install_from_local_copy(self):
-        # self.progress_view.show()
-        self.thread_local_copy_install.start()
+    def _start_apply_changes_subprocedure(self, install_mode: str):
+        # Creating separete thread
+        self.apply_changes_thread = InstallProcedureWorker(
+            function_to_run=self._main_model.apply_changes,
+            keep_packages=self._keep_packages,
+            install_mode=install_mode,
+            local_copy_folder=self._local_copy_folder,
+        )
+        # Connecting its signals
+        self.apply_changes_thread.progress.connect(self._progress_was_made)
+        self.apply_changes_thread.finished.connect(self._thread_stopped_or_terminated)
+        self._progress_view.button_terminate.clicked.connect(
+            self.apply_changes_thread.terminate
+        )
+
+        self._progress_view.show()
+        # TODO: Block certain buttons in the main iterface here to
+        #       prevent from another thread being started. But not
+        #       all of them (eg. log output (aka console)
+        #       should be operational)
+
+        self.apply_changes_thread.start()  # starts the prepared thread
 
     def _progress_was_made(self, progress):
         # TODO: print for test purposes, remove later
         print(f"Current progress (received in adapter's slot): {progress}")
-        self.progress_view.progress_bar.setValue(progress)
+        self._progress_view.progress_bar.setValue(progress)
 
     def _thread_stopped_or_terminated(self):
-        # TODO: this methods is most likely not needed at all
-        # TODO: print for test purposes, remove later
+        self._progress_view.hide()
+        self._progress_view.progress_bar.setValue(0)
         print("Thread finished signal received.")
         print(
-            f"thread: {self.thread_apply_changes}\n"
-            f"is running?: {self.thread_apply_changes.isRunning()}\n"
-            f"is finished?: {self.thread_apply_changes.isFinished()}"
-            "---\n"
-            f"thread: {self.thread_local_copy_install}\n"
-            f"is running?: {self.thread_local_copy_install.isRunning()}\n"
-            f"is finished?: {self.thread_local_copy_install.isFinished()}"
+            f"thread: {self.apply_changes_thread}\n"
+            f"is running?: {self.apply_changes_thread.isRunning()}\n"
+            f"is finished?: {self.apply_changes_thread.isFinished()}"
         )
 
     def _set_keep_packages_state(self):
@@ -173,9 +167,9 @@ class Adapter(QObject):
         print(f"in GUI keep_packages checkbox is set to: {state}")
         # Qt.PartiallyChecked doesn't make sense in this application
         if state == Qt.CheckState.Checked:
-            self._keep_packges = True
+            self._keep_packages = True
         if state == Qt.CheckState.Unchecked:
-            self._keep_packges = False
+            self._keep_packages = False
 
 
 def main():
