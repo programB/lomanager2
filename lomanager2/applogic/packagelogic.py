e42fa6aaafa8f63b582f134fa8fa39855c188ab5
programB <35240807+programB@users.noreply.github.com>
2023-07-09 16:32:57 +0200

    Moves verify_local_copy() subprocedures -> MainLogic

--- lomanager2/applogic/packagelogic.py
+++ lomanager2/applogic/packagelogic.py
@@ -1,4 +1,5 @@
 import time  # TODO: just for the tests
+import re
 import pathlib
 import configuration
 from typing import Any, Tuple
@@ -768,7 +769,6 @@ class MainLogic(object):
         configuration.logging.debug("Cleaning temporary files...")
         time.sleep(1)
         configuration.logging.debug("...done.")
-        # -- end Private methods of MainLogic
 
     def _local_copy_install_procedure(
         self,
@@ -801,7 +801,7 @@ class MainLogic(object):
                 is_LibreOffice_core_present,
                 is_LibreOffice_lang_present,
                 is_Clipart_present,
-            ) = subprocedures.verify_local_copy(local_copy_directory)
+            ) = self._verify_local_copy(local_copy_directory)
         else:
             message = "Could not find directory with saved packages."
             install_status["explanation"] = message
@@ -926,6 +926,113 @@ class MainLogic(object):
         # TODO: should it now converge with the network_install procedure ?
 
         return install_status
+
+    def _verify_local_copy(
+        self,
+        local_copy_directory: str,
+    ) -> tuple[bool, bool, bool, bool]:
+        """Checks for presence of saved packages based on file name convention
+
+        Functions checks for:
+            - 2 Java rpm packages
+            - any LibreOffice core package
+            - LibreOffice langs/help packs folder
+            - 2 clipart rpm packages
+        based on their expected files names (No version checking is performed).
+        Returned is True/False for every of above 4 components.
+        True means component can be installed from local_copy_directory.
+
+        Parameters
+        ----------
+        local_copy_directory : str
+          Directory containing saved packages
+
+        Returns
+        -------
+        tuple[bool,bool,bool,bool]
+          (T) for each: Java, LibreOffice core, LibreOffice langs, Clipart if
+          suitable for install from local_copy_directory (F) otherwise
+        """
+
+        configuration.logging.debug("WIP")
+
+        is_Java_present = False
+        is_LibreOffice_core_present = False
+        is_LibreOffice_lang_present = False
+        is_Clipart_present = False
+
+        configuration.logging.debug("Verifying local copy ...")
+        # 1) Directory for Java rpms exist inside?
+        # (Java_RPMS as directory name is set here as a standard)
+        Java_dir = pathlib.Path(local_copy_directory).joinpath("Java_RPMS")
+        configuration.logging.debug(f"Java RPMS dir: {Java_dir}")
+        if Java_dir.is_dir():
+            # Files: task-java-<something>.rpm ,  java-sun-<something>.rpm
+            # are inside? (no specific version numbers are assumed or checked)
+            is_task_java_present = any(
+                ["task-java" in file.name for file in Java_dir.iterdir()]
+            )
+            is_java_sun_present = any(
+                ["java-sun" in file.name for file in Java_dir.iterdir()]
+            )
+            # Only if both are present we conclude Java can be installed
+            # from the local copy directory.
+            is_Java_present = all([is_task_java_present, is_java_sun_present])
+
+        # 2) LibreOffice core packages folder
+        LO_core_dir = pathlib.Path(local_copy_directory).joinpath("LO_core_TGZS")
+        if LO_core_dir.is_dir():
+            # Check for tar.gz archive with core packages
+            regex = re.compile(
+                "LibreOffice_[0-9]*.[0-9]*.[0-9]*_Linux_x86-64_rpm.tar.gz"
+            )
+            is_LibreOffice_core_present = any(
+                [regex.search(file.name) for file in LO_core_dir.iterdir()]
+            )
+
+        # 3) LibreOffice lang and help packages folder
+        #    (its content is not critical for the decision procedure
+        #     so just check if it exists and is non empty)
+        LO_lang_dir = pathlib.Path(local_copy_directory).joinpath("LO_lang_TGZS")
+        is_LibreOffice_lang_present = LO_lang_dir.is_dir() and any(
+            LO_lang_dir.iterdir()
+        )
+
+        # 4) Clipart directory
+        Clipart_dir = pathlib.Path(local_copy_directory).joinpath("Clipart_RPMS")
+        if Clipart_dir.is_dir():
+            # Files: libreoffice-openclipart-<something>.rpm ,
+            # clipart-openclipart-<something>.rpm
+            # are inside? (no specific version numbers are assumed or checked)
+            is_lo_clipart_present = any(
+                [
+                    "libreoffice-openclipart" in file.name
+                    for file in Clipart_dir.iterdir()
+                ]
+            )
+            is_openclipart_present = any(
+                ["clipart-openclipart" in file.name for file in Clipart_dir.iterdir()]
+            )
+            # Only if both are present clipart library can be installed
+            # from the local copy directory.
+            is_Clipart_present = all([is_lo_clipart_present, is_openclipart_present])
+
+        configuration.logging.debug(f"Java is present: {is_Java_present}")
+        configuration.logging.debug(
+            f"LO core is present: {is_LibreOffice_core_present}"
+        )
+        configuration.logging.debug(
+            f"LO langs is present: {is_LibreOffice_lang_present}"
+        )
+        configuration.logging.debug(f"Clipart lib is present: {is_Clipart_present}")
+        return (
+            is_Java_present,
+            is_LibreOffice_core_present,
+            is_LibreOffice_lang_present,
+            is_Clipart_present,
+        )
+
+    # -- end Private methods of MainLogic
 
 
 class PackageMenu(object):
