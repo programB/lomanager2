6384f475142fd537850f56aba65008e9eea72204
programB <35240807+programB@users.noreply.github.com>
2023-07-09 16:23:51 +0200

    Moves local_copy_install_procedure subprocedures -> MainLogic

--- lomanager2/applogic/packagelogic.py
+++ lomanager2/applogic/packagelogic.py
@@ -1,4 +1,5 @@
 import time  # TODO: just for the tests
+import pathlib
 import configuration
 from typing import Any, Tuple
 from . import PCLOS
@@ -199,11 +200,10 @@ class MainLogic(object):
         )
         local_copy_directory = "/tmp/saved_packages"
 
-        status = subprocedures.local_copy_install_procedure(
+        status = self._local_copy_install_procedure(
             self._virtual_packages,
             tmp_directory,
             keep_packages=True,  # never delete local copy provided by the user
-            install_mode="local_copy_install",
             local_copy_directory=local_copy_directory,
             callback_function=callback_function,
         )
@@ -522,9 +522,7 @@ class MainLogic(object):
         }
 
         # 1 - Run collect_packages subprocedure
-        packages_to_download = [
-            p for p in virtual_packages if p.is_to_be_downloaded
-        ]
+        packages_to_download = [p for p in virtual_packages if p.is_to_be_downloaded]
         configuration.logging.debug(f"packages_to_download: {packages_to_download}")
 
         # TODO: Should it be a different function or perhaps
@@ -771,6 +769,163 @@ class MainLogic(object):
         time.sleep(1)
         configuration.logging.debug("...done.")
         # -- end Private methods of MainLogic
+
+    def _local_copy_install_procedure(
+        self,
+        virtual_packages,
+        tmp_directory,
+        keep_packages,
+        local_copy_directory,
+        callback_function=None,
+    ) -> dict:
+        # TODO: This is dummy implementation for testing
+        configuration.logging.debug("WIP !")
+
+        # Preparations
+
+        current_progress_is = callback_function
+        install_status = {
+            "is_install_successful": False,
+            "explanation": "Install procedure not executed.",
+        }
+
+        # Perform rough verification of local copy directory
+        is_Java_present = False
+        is_LibreOffice_core_present = False
+        is_LibreOffice_lang_present = False
+        is_Clipart_present = False
+        # local_copy_directory exists?
+        if pathlib.Path(local_copy_directory).is_dir():
+            (
+                is_Java_present,
+                is_LibreOffice_core_present,
+                is_LibreOffice_lang_present,
+                is_Clipart_present,
+            ) = subprocedures.verify_local_copy(local_copy_directory)
+        else:
+            message = "Could not find directory with saved packages."
+            install_status["explanation"] = message
+            configuration.logging.error(message)
+            return install_status
+
+        # Mark virtual packages accordingly to the
+        # content of local_copy_directory and system state
+        if is_LibreOffice_core_present is False:
+            # There are no LibreOffice core packages in local_copy_directory but
+            # perhaps the user meant to install Openclipart library?
+            if is_Clipart_present is False:
+                message = (
+                    "Neither LibreOffice nor Openclipart library were found "
+                    "in the folder provided."
+                )
+                install_status["explanation"] = message
+                configuration.logging.error(message)
+                return install_status
+            else:  # Clipart packages found in local_copy_directory
+                # FIXME: Currently there is no guarantee clipart virtual package
+                #       will be in virtual_packages.
+                #       This is a problem and must be fixed in PackageMenu
+                #       And what if it exists? Should it be upgraded ?
+                configuration.logging.debug(
+                    "Openclipart rpms found and marked for installation."
+                )
+                for package in virtual_packages:
+                    if package.family == "Clipart":
+                        package.is_marked_for_removal = False
+                        package.is_marked_for_upgrade = False
+                        package.is_marked_for_install = True
+                        package.is_to_be_downloaded = False
+
+        if is_LibreOffice_core_present is True:
+            # Assuming user wants to install LibreOffice from local_copy_directory.
+            # This is possible only if Java is present in the OS or
+            # can be installed from local_copy_directory
+            if PCLOS.is_java_installed() is False and is_Java_present is False:
+                message = (
+                    "Java is not installed in the system and was not be found in "
+                    "the directory provided."
+                )
+                install_status["explanation"] = message
+                configuration.logging.error(message)
+                return install_status
+            elif PCLOS.is_java_installed() is False and is_Java_present is True:
+                # Java not installed but can be installed from local_copy_directory
+                # FIXME: Java virtual package in not guaranteed to exist in
+                #        virtual_packages. FIX this.
+                configuration.logging.debug(
+                    "Java rpms found and marked for installation."
+                )
+                for package in virtual_packages:
+                    if package.family == "Java":
+                        package.is_marked_for_removal = False
+                        package.is_marked_for_upgrade = False
+                        package.is_marked_for_install = True
+                        package.is_to_be_downloaded = False
+
+            elif PCLOS.is_java_installed() is True and is_Java_present is False:
+                configuration.logging.debug("Java already installed.")
+                for package in virtual_packages:
+                    if package.family == "Java":
+                        package.is_marked_for_removal = False
+                        package.is_marked_for_upgrade = False
+                        package.is_marked_for_install = False
+                        package.is_to_be_downloaded = False
+            else:
+                # TODO Java is installed AND is present in local_copy_directory
+                #      what should be done it such a case? Reinstall?
+                #      Try using rpm -Uvh which will update it if package is newer?
+                #      Skipping for now.
+                pass
+
+            configuration.logging.debug(
+                "Marking all existing OpenOffice and LibreOffice packages for removal."
+            )
+            for package in virtual_packages:
+                if package.family == "OpenOffice" or package.family == "LibreOffice":
+                    package.is_marked_for_removal = True
+                    package.is_marked_for_upgrade = False
+                    package.is_marked_for_install = False
+                    package.is_to_be_downloaded = False
+
+            configuration.logging.debug("Marking LibreOffice core for install.")
+            # FIXME: Which package to mark? virtual_packages only contains
+            #        list of installed packages (which by itself should be changes
+            #        for different reason) and the version number of the
+            #        LO core in the local_copy_directory is unknown.
+            #        Parse the filename? add "0.0.0.0" as indicating local copy
+            #        install?
+            for package in virtual_packages:
+                if package.family == "OpenOffice" and package.kind == "core-packages":
+                    package.is_marked_for_removal = False
+                    package.is_marked_for_upgrade = False
+                    package.is_marked_for_install = True
+                    package.is_to_be_downloaded = False
+
+            configuration.logging.debug("DO SOMETHING HERE")
+            if is_LibreOffice_lang_present:
+                # User has also saved some language packs. Install them all.
+                # TODO: mark them for install but not for download
+                # FIXME: Again which packages to mark? virtual_packages only contains
+                #        list of installed packages (which by itself should be changes
+                #        for different reason) and the version number of the
+                #        LO core in the local_copy_directory is unknown.
+                #        Parse the filename? add "0.0.0.0" as indicating local copy
+                #        install?
+                configuration.logging.debug("DO SOMETHING HERE")
+            if is_Clipart_present:
+                # User has also saved clipart package. Install it.
+                # TODO: mark clipart for installation/upgrade accordingly
+                # FIXME: Again which package to mark? virtual_packages only contains
+                #        list of installed packages (which by itself should be changes
+                #        for different reason) and the version number of the
+                #        LO core in the local_copy_directory is unknown.
+                #        Parse the filename? add "0.0.0.0" as indicating local copy
+                #        install?
+                configuration.logging.debug("DO SOMETHING HERE")
+
+        # TODO: should it now converge with the network_install procedure ?
+
+        return install_status
 
 
 class PackageMenu(object):
