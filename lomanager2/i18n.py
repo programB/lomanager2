157c6d409f0e79f17d83c05958c548df24428127
programB <35240807+programB@users.noreply.github.com>
2023-09-28 13:05:14 +0200

    Makes gettext look for translations in /usr/share/locale

--- lomanager2/i18n.py
+++ lomanager2/i18n.py
@@ -1,4 +1,14 @@
 import gettext
 
-t = gettext.translation("lomanager2", localedir="./locales", fallback=True)
+# t = gettext.translation("lomanager2", localedir="./locales", fallback=True)
+
+# When localedir is not specified gettext will, by default, look for
+# translation files (.mo) in /usr/share/locale/<lang code>/LC_MESSAGES
+# where lang code is searched for in the environment variables:
+# LANGUAGE, LC_ALL, LC_MESSAGES, and LANG respectively. First found is picked.
+# Fallback is to other existing translations that exist an were set in
+# environment variables. eg. if LANGUAGE = de_DE;it_IT and German .mo
+# is not present in LC_MESSAGES but Italian was found it will be used as
+# fallback. The ultimate fallback are the raw, untranslated strings.
+t = gettext.translation("lomanager2", fallback=True)
 _ = t.gettext
