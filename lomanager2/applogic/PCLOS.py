9b135ea9083b3543f881f8df6bea1a151f5f6ebf
programB <35240807+programB@users.noreply.github.com>
2023-09-30 14:54:15 +0200

    Adds copyright notice and GPL3 copying permissions to every file

--- lomanager2/applogic/PCLOS.py
+++ lomanager2/applogic/PCLOS.py
@@ -1,6 +1,19 @@
 """
-This module gathers functions that return some information about current
-operating system state or change that state.
+Copyright (C) 2023 programB
+
+This file is part of lomanager2.
+
+lomanager2 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3
+as published by the Free Software Foundation.
+
+lomanager2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with lomanager2.  If not, see <http://www.gnu.org/licenses/>.
 """
 import logging
 import os
